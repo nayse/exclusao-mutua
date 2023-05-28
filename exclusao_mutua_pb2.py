fd680367ca1c6fcdf1d7c2befcd19e3758e0b7bc
nayse <37347392+nayse@users.noreply.github.com>
2023-05-28 00:44:46 -0300

    Update exclusao_mutua_pb2.py

--- exclusao_mutua_pb2.py
+++ exclusao_mutua_pb2.py
@@ -13,17 +13,25 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x14\x65xclusao_mutua.proto\x12\x0e\x65xclusao_mutua\"\'\n\rPedidoRequest\x12\x16\n\x0eid_do_processo\x18\x01 \x01(\x05\"\x10\n\x0ePedidoResponse2\xb5\x01\n\x0eServicoEleicao\x12P\n\rIniciarPedido\x12\x1d.exclusao_mutua.PedidoRequest\x1a\x1e.exclusao_mutua.PedidoResponse\"\x00\x12Q\n\x0e\x45ncerrarPedido\x12\x1d.exclusao_mutua.PedidoRequest\x1a\x1e.exclusao_mutua.PedidoResponse\"\x00\x62\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x14\x65xclusao_mutua.proto\x12\x0e\x65xclusao_mutua\"(\n\x12RequisicaoRegistro\x12\x12\n\ncliente_id\x18\x01 \x01(\x05\"\x12\n\x10RespostaRegistro\"&\n\x10RequisicaoAcesso\x12\x12\n\ncliente_id\x18\x01 \x01(\x05\"#\n\x0eRespostaAcesso\x12\x11\n\tpermitido\x18\x01 \x01(\x08\")\n\x13RequisicaoLiberacao\x12\x12\n\ncliente_id\x18\x01 \x01(\x05\"\x13\n\x11RespostaLiberacao2\x9d\x02\n\rExclusaoMutua\x12Z\n\x10RegistrarCliente\x12\".exclusao_mutua.RequisicaoRegistro\x1a .exclusao_mutua.RespostaRegistro\"\x00\x12U\n\x0fSolicitarAcesso\x12 .exclusao_mutua.RequisicaoAcesso\x1a\x1e.exclusao_mutua.RespostaAcesso\"\x00\x12Y\n\rLiberarAcesso\x12#.exclusao_mutua.RequisicaoLiberacao\x1a!.exclusao_mutua.RespostaLiberacao\"\x00\x62\x06proto3')
 
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'exclusao_mutua_pb2', globals())
 if _descriptor._USE_C_DESCRIPTORS == False:
 
   DESCRIPTOR._options = None
-  _PEDIDOREQUEST._serialized_start=40
-  _PEDIDOREQUEST._serialized_end=79
-  _PEDIDORESPONSE._serialized_start=81
-  _PEDIDORESPONSE._serialized_end=97
-  _SERVICOELEICAO._serialized_start=100
-  _SERVICOELEICAO._serialized_end=281
+  _REQUISICAOREGISTRO._serialized_start=40
+  _REQUISICAOREGISTRO._serialized_end=80
+  _RESPOSTAREGISTRO._serialized_start=82
+  _RESPOSTAREGISTRO._serialized_end=100
+  _REQUISICAOACESSO._serialized_start=102
+  _REQUISICAOACESSO._serialized_end=140
+  _RESPOSTAACESSO._serialized_start=142
+  _RESPOSTAACESSO._serialized_end=177
+  _REQUISICAOLIBERACAO._serialized_start=179
+  _REQUISICAOLIBERACAO._serialized_end=220
+  _RESPOSTALIBERACAO._serialized_start=222
+  _RESPOSTALIBERACAO._serialized_end=241
+  _EXCLUSAOMUTUA._serialized_start=244
+  _EXCLUSAOMUTUA._serialized_end=529
 # @@protoc_insertion_point(module_scope)
