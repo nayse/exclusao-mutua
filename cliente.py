208068e74eabc13f54a76fd1e88d648e92ad1d62
nayse <37347392+nayse@users.noreply.github.com>
2023-05-28 00:43:37 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -1,20 +1,33 @@
 import grpc
 import exclusao_mutua_pb2
 import exclusao_mutua_pb2_grpc
-import time
 
-def cliente():
-    channel = grpc.insecure_channel('localhost:50051')
-    stub = exclusao_mutua_pb2_grpc.ServicoEleicaoStub(channel) 
-    request = exclusao_mutua_pb2.PedidoRequest(id_do_processo=1)     #envia pedido p/ iniciar a exclusao mutua
-    response = stub.IniciarPedido(request)
-    print("o processo --- {} --- iniciou o pedido de exclusão mútua".format(request.id_do_processo))
-    request = exclusao_mutua_pb2.PedidoRequest(id_do_processo=1)     
-    response = stub.EncerrarPedido(request) #envia pedido p/ acessar o recurso compartilhado
-    print("o processo --- {} --- acessou o recuso compartilhado".format(request.id_do_processo))
-    request = exclusao_mutua_pb2.PedidoRequest(id_do_processo=1) 
-    response = stub.EncerrarPedido(request) # envia pedido p/ liberar o recurso
-    print("o processo --- {} --- encerrou pediu exclusão mútua".format(request.id_do_processo))
+def run_client():
+
+    channel = grpc.insecure_channel('localhost:90000')
+
+    stub = exclusao_mutua_pb2_grpc.ExclusaoMutuaStub(channel)
+
+    cliente_id = 123
+    op = False
+    while op == False:
+        requisicao_acesso = exclusao_mutua_pb2.RequisicaoAcesso(cliente_id=cliente_id)
+
+
+        resposta_acesso = stub.SolicitarAcesso(requisicao_acesso)
+
+
+        if resposta_acesso.permitido:
+            print(f"Cliente {cliente_id} obteve acesso à seção crítica.")
+            liberacao = input("Digite 'l' para liberar o acesso: ")
+            if liberacao == 'l':
+                requisicao_liberacao = exclusao_mutua_pb2.RequisicaoLiberacao(cliente_id=cliente_id)
+                stub.LiberarAcesso(requisicao_liberacao)
+                print(f"Cliente {cliente_id} liberou o acesso à seção crítica.")
+                print()
+            op = True
+        else:
+            print(f"Acesso negado para o cliente {cliente_id}. Aguardando para obter acesso à seção crítica.")
 
 if __name__ == '__main__':
-    cliente()
+    run_client()
